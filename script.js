184e5ac63fda85a06c206ef650a00c2840f3f551
KevDCS <kcruzs@outlook.com>
2024-05-20 19:05:55 +0200

    Se hizo el script para agregar los ejercicios al aside al hacer click en cada uno de ellos, y tambien que se muestre una alerta cuando se quiera seleccionar dos veces el mismo ejercicio.

--- script.js
+++ script.js
@@ -18,3 +18,36 @@ function openTab(evt, tabName) {
 	document.getElementById(tabName).style.display = "block";
 	evt.currentTarget.className += " active";
 }
+
+// Función para abrir la pestaña que tenga el ID defaultOpen por defecto
+document.getElementById("defaultOpen").click();
+
+// Agregar un event listener en elementos que tengan el id tab_item
+const element = document.getElementsByClassName("tab_item");
+
+// Seleccionar el aside donde se enlistaran los titulos de los ejercicios
+const listaEjercicios = document.getElementById("lista-ejercicios");
+
+for(let i = 0; i < element.length; i++) {
+	element[i].addEventListener("click", () => {
+		const titulo = element[i].querySelector('span').textContent;
+
+    let yaExiste = false;
+    const items =listaEjercicios.getElementsByTagName('li');
+
+    for(let j = 0; j < items.length; j++) {
+      if(items[j].textContent === titulo) {
+        yaExiste = true;
+        window.alert("Ejercicio " + titulo + " ya ha sido agregado");
+        break;
+      }
+    }
+
+    if (!yaExiste) {
+      // Agregar el elemento a la lista
+      const li = document.createElement('li');
+      li.textContent = titulo;
+      listaEjercicios.appendChild(li);
+    }
+	});
+}
